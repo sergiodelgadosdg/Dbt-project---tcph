05da81e8365fe5b2a93f2e6cbcb1c8b9f02d0fad
sergio.delgado@sdggroup.com <sergio.delgado@sdggroup.com>
2025-10-16 10:03:50 +0000

    errors date references and incremental

--- models/marts/business/fct_orders.sql
+++ models/marts/business/fct_orders.sql
@@ -7,24 +7,23 @@
     )
 }}
 
-with orders as (
+with new_orders as (
     select *
     from {{ ref('int_orders') }}
     {% if is_incremental() %}
-        -- Solo traemos nuevas órdenes o actualizadas
         where order_date > (select max(order_date) from {{ this }})
     {% endif %}
 ),
 
 orders_enriched as (
     select
-        {{ dbt_utils.generate_surrogate_key(['order_id', 'line_number', 'part_id'])}} as orders_key,
+        {{ dbt_utils.generate_surrogate_key(['order_id', 'line_number', 'part_id']) }} as orders_key,
         {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as customer_key,
         {{ dbt_utils.generate_surrogate_key(['nation_id']) }} as nation_key,
         {{ dbt_utils.generate_surrogate_key(['part_id']) }} as part_key,
         {{ dbt_utils.generate_surrogate_key(['supplier_id']) }} as supplier_key,
-        {{ dbt_utils.generate_surrogate_key(['order_date']) }} as order_date_key,
-        {{ dbt_utils.generate_surrogate_key(['ship_date']) }} as ship_date_key,
+        order_date,
+        ship_date,
         quantity,
         extended_price,
         discount,
@@ -38,14 +37,12 @@ orders_enriched as (
         clerk_id,
         order_priority,
         supply_cost,
-        round({{ calc_final_price('extended_price', 'discount', 'tax') }}, 2) as final_price,
+        round((extended_price * (1 - discount)) * (1 + tax), 2) as final_price,
         round((supply_cost * quantity), 2) as total_cost,
-        round({{ calc_final_price('extended_price', 'discount', 'tax') }} - (supply_cost * quantity), 2) as profit,
+        round((extended_price * (1 - discount)) * (1 + tax) - (supply_cost * quantity), 2) as profit,
         greatest(datediff(day, commit_date, ship_date), 0) as days_of_delay,
         datediff(day, ship_date, receipt_date) as shipping_days
-    from orders
+    from new_orders
 )
 
-
-select *
-from orders_enriched
+select * from orders_enriched
