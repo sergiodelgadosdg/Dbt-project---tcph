e7c845e8ffbc3d40504a178a4b518def80070097
sergio.delgado@sdggroup.com <sergio.delgado@sdggroup.com>
2025-10-16 09:20:59 +0000

    surrogate keys and references

--- models/marts/business/fct_partsupp.sql
+++ models/marts/business/fct_partsupp.sql
@@ -2,8 +2,8 @@ with partsupp as (
 
     select
         {{ dbt_utils.generate_surrogate_key(['part_id', 'supplier_id'])}} as partsupp_key,
-        part_id,
-        supplier_id,
+        {{ dbt_utils.generate_surrogate_key(['part_id']) }} as part_key,
+        {{ dbt_utils.generate_surrogate_key(['supplier_id']) }} as supplier_key,
         supply_cost,
         availability
     from {{ ref('stg_partsupp') }}
