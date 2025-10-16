0b13e55aa58e114b7a14d0ed45e6684f3db970a3
sergio.delgado@sdggroup.com <sergio.delgado@sdggroup.com>
2025-10-16 15:11:35 +0000

    changed unique key incremental

--- models/marts/business/fct_orders.sql
+++ models/marts/business/fct_orders.sql
@@ -1,7 +1,7 @@
 {{ 
     config(
         materialized='incremental',
-        unique_key='order_id',
+        unique_key='orders_key',
         incremental_strategy='merge',
         on_schema_change='sync',
         post_hook= "grant select on {{ this }} to role SYSADMIN"
